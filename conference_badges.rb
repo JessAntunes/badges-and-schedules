a1cdc55b596ba9061968cea42b953e8d7e7536c2
Jessica Antunes <jessantunes@protonmail.com>
2021-02-24 15:55:21 -0500

    commit tree c3824bdba2cee4c9241b94c5116bca7bd3c30387

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+   
+
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |name|
+        badges.push("Hello, my name is #{name}.")
+    end
+    return badges
+end
+
+def assign_rooms(attendees)
+    room_assignments = []
+    attendees.each.with_index(1) do |name, room_number| 
+        room_assignments.push("Hello, #{name}! You'll be assigned to room #{room_number}!")
+    end
+    return room_assignments
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees)
+    assign_rooms(attendees)
+    badges.each do |badge|
+        puts badge
+    end
+    room_assignments.each do |room|
+        puts room
+    end
+end
